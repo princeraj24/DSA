46d27fafbe1d0c383f562c25e0c4ab7e5c6f9296
princeraj24 <152485826+princeraj24@users.noreply.github.com>
2026-07-30 16:13:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (54.67%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+
+        if(n <= 8){
+            return n;
+        }
+
+        if(n > 8 && n <= 16){
+            return 8 + (n - 8) * 2;
+        }
+
+        else if(n > 16 && n <= 24){
+            return 8 + 8 * 2 + (n - 16) * 3;
+        }
+
+        else{
+            return 8 + 8 * 2 + 8 * 3 + (n - 24) * 4;
+        }
+    }
+};

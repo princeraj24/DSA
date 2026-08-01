2c594d398c537e12fd03dfd7dc0bd22516df1ce8
princeraj24 <152485826+princeraj24@users.noreply.github.com>
2026-08-02 00:18:27 +0530

    Update predict-the-winner.cpp

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -3,6 +3,8 @@
     -When it's your turn, do your best(take max())
     -When it's your opponet's turn, expect the worst case (that's why min() is taken below)
 */
+// Approach 1
+// Finding player1_score and check with player2_score at end
 class Solution {
 public:
     int t[21][21];
@@ -38,5 +40,38 @@ public:
         int player2_score = total_score - player1_score;
 
         return player1_score >= player2_score;
+    }
+};
+
+
+// Approach 2
+// Player1_Score - Player2_Score >= 0, then it's valid
+class Solution {
+public:
+    int t[21][21];
+    // Difference nikalne ke liye scores ke
+    int solve(int i, int j, vector<int>& nums){
+        if(i > j){
+            return 0;
+        }
+
+        if(i == j){
+            return nums[i];
+        }
+
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        int take_i = nums[i] - solve(i + 1, j, nums);
+        int take_j = nums[j] - solve(i, j - 1, nums);
+
+        return t[i][j] = max(take_i, take_j);
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        memset(t, -1, sizeof(t));
+        int n = nums.size();
+
+        return solve(0, n-1, nums) >= 0;
     }
 };

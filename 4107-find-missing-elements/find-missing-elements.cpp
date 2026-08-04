9ac500cac84617f5a1cd42c5c796f1523e8654fd
princeraj24 <152485826+princeraj24@users.noreply.github.com>
2026-08-04 11:36:17 +0530

    Time: 0 ms (100.00%) | Memory: 32.3 MB (54.61%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -3,22 +3,22 @@ public:
     vector<int> findMissingElements(vector<int>& nums) {
         int n = nums.size();
 
-        int mini = INT_MAX;
-        int maxi = INT_MIN;
+        int mini = 101;
+        int maxi = -1;
         for(int val : nums){
             mini = min(mini, val);
             maxi = max(maxi, val);
         }
         
-        unordered_map<int, int> freq;
+        vector<bool> present(maxi - mini + 1, false);
         for(int val : nums){
-            freq[val]++;
+            present[val - mini] = true;
         }
 
         vector<int> ans;
-        for(int x = mini + 1; x < maxi; x++){
-            if(freq.find(x) == freq.end()){
-                ans.push_back(x);
+        for(int x = 1; x < present.size(); x++){
+            if(!present[x]){
+                ans.push_back(x + mini);
             }
         }
 

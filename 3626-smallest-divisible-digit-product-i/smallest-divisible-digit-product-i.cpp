0f700a42814fa74665188c2c3b91599284bf213f
princeraj24 <152485826+princeraj24@users.noreply.github.com>
2026-08-07 11:24:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (38.10%) - LeetSync

--- 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -5,15 +5,19 @@ public:
         while(num != 0){
             int digit = num % 10;
 
+            if(digit == 0){
+                return true;
+            }
+
             product = product * digit;
 
             num = num / 10;
-        }
+        }   
 
         return (product % target == 0);
     }
     int smallestNumber(int n, int t) {
-        for(int i = n; i <= 100; i++){
+        for(int i = n; i <= n + 10; i++){
             if(checkDigitProduct(i, t)){
                 return i;
             }

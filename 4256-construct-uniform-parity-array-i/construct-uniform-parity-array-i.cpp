a37594088c82891a40bf5c28be4cc3072db32dcd
princeraj24 <152485826+princeraj24@users.noreply.github.com>
2026-09-02 11:57:57 +0530

    Time: 3 ms (5.87%) | Memory: 30.2 MB (94.95%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int n = nums1.size();
-        bool allOdd = true;
-        bool allEven = true;
-        int lastOdd = -1;
+        // int n = nums1.size();
+        // bool allOdd = true;
+        // bool allEven = true;
+        // int lastOdd = -1;
 
-        for(int i =0; i < n; i++){
-            // Even check
-            if(nums1[i] % 2 == 0){
-                allOdd = false;
-            }
+        // for(int i =0; i < n; i++){
+        //     // Even check
+        //     if(nums1[i] % 2 == 0){
+        //         allOdd = false;
+        //     }
 
-            // Odd check 
-            else{
-                allEven = false;
-                lastOdd = nums1[i];
-            }
-        }
+        //     // Odd check 
+        //     else{
+        //         allEven = false;
+        //         lastOdd = nums1[i];
+        //     }
+        // }
 
         // if(allOdd == true || allEven == true){
         //     return true;

485ceebcf036920acbf9b8bed61a66a9a5d9fd39
princeraj24 <152485826+princeraj24@users.noreply.github.com>
2026-09-02 11:56:55 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (67.80%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -11,6 +11,7 @@ public:
             if(nums1[i] % 2 == 0){
                 allOdd = false;
             }
+
             // Odd check 
             else{
                 allEven = false;
@@ -20,31 +21,6 @@ public:
 
         if(allOdd == true || allEven == true){
             return true;
-        }
-
-        vector<bool> nums2(n, false);
-        int i = 0;
-        
-        for(int j = 0; j < n; j++){
-            // odd
-            if(nums1[i] % 2 == 1){
-                nums2[j] = true;
-                i++;
-            }
-            // even
-            else{
-                if(lastOdd != -1){
-                    nums2[i] = true;
-                    i++; 
-                }
-                
-            }
-        }
-
-        for(bool isUniform : nums2){
-            if(isUniform == false){
-                return false;
-            }
         }
 
         return true;

b6ed5f18549c53ba6ddf63da479370e91bb18411
princeraj24 <152485826+princeraj24@users.noreply.github.com>
2026-09-11 18:53:41 +0530

    Time: 7 ms (65.69%) | Memory: 30.1 MB (77.55%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -2,29 +2,40 @@ class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
         int n = digits.size();
-        set<int> st;
+        unordered_map<int, int> mp;
+        vector<int> result;
 
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                for(int k = 0; k < n; k++){
-                    if(i == j || j == k || k == i){
-                        continue;
-                    }
-
-                    if(digits[i] == 0){
-                        continue;
-                    }
-
-                    if(digits[k] % 2 != 0){
-                        continue;
-                    }
-
-                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
-                    st.insert(num);
-                }
-            }
+            mp[digits[i]]++;
         }
 
-        return st.size();
+        for(int i = 1; i <= 9; i++){
+            if(mp[i] == 0){
+                continue;
+            }
+            mp[i]--;
+            for(int j = 0; j <= 9; j++){
+                if(mp[j] == 0){
+                    continue;
+                }
+                mp[j]--;
+                for(int k = 0; k <= 8; k+=2){
+                    if(mp[k] == 0){
+                        continue;
+                    }
+                    mp[k]--;
+
+                    int num = (i * 100) + (j * 10) + k;
+                    result.push_back(num);
+
+                    mp[k]++;
+                }
+
+                mp[j]++;
+            }
+            mp[i]++;
+        }
+
+        return result.size();
     }
 };

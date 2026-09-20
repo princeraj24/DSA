712842da3e5d5e36bb0035c9bbdc191de99e9ba7
princeraj24 <152485826+princeraj24@users.noreply.github.com>
2026-09-20 23:29:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (34.96%) - LeetSync

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -1,17 +1,48 @@
 class Solution {
 public:
-    bool checkOverlap(int radius, int xCenter, int yCenter,
-                      int x1, int y1, int x2, int y2) {
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        // Method - 1
+        // int closest_X;
+        // int closest_Y;
 
-        // Find the closest point of rectangle to circle center
-        int closestX = max(x1, min(xCenter, x2));
-        int closestY = max(y1, min(yCenter, y2));
+        // if(x1 > xCenter){
+        //     closest_X = x1;
+        // }
 
-        // Calculate squared distance
-        int dx = xCenter - closestX;
-        int dy = yCenter - closestY;
+        // else if(x2 < xCenter){
+        //     closest_X = x2;
+        // }
 
-        // Check if closest point is inside/on circle
+        // else{
+        //     closest_X = xCenter;
+        // }
+
+        // if(y1 > yCenter){
+        //     closest_Y = y1;
+        // }
+
+        // else if(y2 < yCenter){
+        //     closest_Y = y2;
+        // }
+
+        // else{
+        //     closest_Y = yCenter;
+        // }
+
+
+        // Method - 2
+        // use clamp(value, lo, hi) inbuilt c++ function
+        // lo : if(lo > value)
+        // hi : if(hi > value)
+        // else value
+
+        int closest_X = clamp(xCenter, x1, x2);
+        int closest_Y = clamp(yCenter, y1, y2);
+
+
+        int dx = xCenter - closest_X; // here we don't consider negative value as we are calculating square of distance
+        int dy = yCenter - closest_Y;
+
         return dx * dx + dy * dy <= radius * radius;
     }
 };
